97d2a372a1e83be4410b0d0d10f81f0fcdee2c67
MingRone08 <yuenbinhminh@gmail.com>
2022-05-18 21:03:05 +0700

    buoi8_react_form

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -2,16 +2,20 @@ import { combineReducers, createStore } from 'redux'
 import { gameXucXacReducer } from './reducers/gameXucXacReducer';
 import { gioHangReducer } from './reducers/gioHangReducer';
 import { stateNumber } from './reducers/numberReducer';
+import { quanLyNguoiDungReducer } from './reducers/quanLyNguoiDungReducer';
 import { stateImgCar } from './reducers/stateImgCarReducer';
 
-
-
 const rootReducer = combineReducers({
-	//các state dự án sẽ được khai báo tại đây
-	stateNumber: stateNumber,
-	stateImgCar,
-	gioHangReducer,
-	gameXucXacReducer
+    //các state dự án sẽ được khai báo tại đây
+    stateNumber: stateNumber,
+    stateImgCar: stateImgCar,
+    gioHangReducer:gioHangReducer,
+    gameXucXacReducer,
+    quanLyNguoiDungReducer:quanLyNguoiDungReducer
+
+
+
 });
+
 
 export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
